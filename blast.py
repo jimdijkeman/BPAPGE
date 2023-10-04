cb9862edb325040af97242d2a24167d84971dd2c
Jim van Dijk <jimdijkemans@pop-os.localdomain>
2023-10-05 00:23:14 +0200

    ok

--- blast.py
+++ blast.py
@@ -2,6 +2,8 @@ import subprocess
 import json
 from pprint import pprint
 
+from Bio import SeqIO
+
 class BLAST:
     def __init__(self, blast_type: str, query_file: str, db_file: str,
                  output_fmt: str = "6", output_file: str = "data/out/blastresults.out", **kwargs) -> None:
@@ -14,7 +16,7 @@ class BLAST:
 
     def run(self):
         command = f"{self.blast_type} -db {self.db_file} -query {self.query_file} -outfmt {self.output_fmt} -out {self.output_file}"
-        
+
         for key, value in self.extra_args.items():
             command += f" -{key} {value}"
 
@@ -30,18 +32,24 @@ class BlastJSONParser:
     def __init__(self, blast_result):
         self.blast_result = blast_result
 
+    @staticmethod
+    def get_sequence_from_file(fasta_file: str, search_id: str):
+        with open(fasta_file, "r") as file:
+            for seq in SeqIO.parse(file, "fasta"):
+                if seq.id == search_id:
+                    return seq.seq
+            return None
+
     def parse(self):
         definitive_results = []
         with open(self.blast_result, 'r') as file:
             data = json.loads(file.read())
             for report in data['BlastOutput2']:
                 results = report['report']['results']
+                query_title = results['search']['query_title']
                 sorted_hits = sorted(results['search']['hits'], key=lambda x: x['hsps'][0]['evalue'])
                 highest_score = sorted_hits[0]
-
-                #if highest_score['hsps'][0]['identity'] == highest_score['hsps'][0]['align_len']:
+                highest_score['query_title'] = query_title
+                highest_score['original_nuc_seq'] = str(self.get_sequence_from_file('seq.fa', query_title)) 
                 definitive_results.append(highest_score)
-
-            print(len(definitive_results))
-
-                #break
+            return definitive_results
